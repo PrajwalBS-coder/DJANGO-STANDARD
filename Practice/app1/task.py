39ae901eb20eb6618933d17744e269c1f0c1cce3
Prajwal B S <prajwaljhon@gmail.com>
2025-04-28 22:27:18 +0530

    redis working one task "celery -A Practice worker -l info --pool=solo" using this commnad to run worker

--- Practice/app1/task.py
+++ Practice/app1/task.py
@@ -1,10 +1,12 @@
 from celery import shared_task 
-
 from time import sleep
+from celery.utils.log import get_task_logger
+
+logger = get_task_logger(__name__)
 
 @shared_task
 def my_task():
     for i in range(11):
-        print(i)
+        logger.info(f"Progress: {i}")
         sleep(1)
     return "Task Complete!"
