2c7bf9586bc59baf69dfe81ecffd083e3c2f00d9
Prajwal B S <prajwaljhon@gmail.com>
2025-05-16 17:07:12 +0530

    Configured model for IMages

--- Practice/apps/app3/models.py
+++ Practice/apps/app3/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Images(models.Model):
+    name = models.CharField(max_length=100,
+                            null=True,
+                            blank=True,
+                            db_column="Name of File",
+                            db_comment='image name')
+    file = models.FileField(upload_to='images/',
+                            null=True,
+                            blank=True,
+                            verbose_name='Image Files')
+    def __str__(self):
+        return self.name
